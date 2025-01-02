6deba0c163b3b9d3a4c4c93fc91bbc7554a6c4cb
Aminu Muhammad <105592867+Elvmeen@users.noreply.github.com>
2025-01-02 18:32:10 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-from func import * 
+from func import post_generated_tweet  # Import the function that generates and posts the tweet
 
 if __name__ == "__main__":
-   post_date()
+    post_generated_tweet()  # Call the function to generate and post the tweet
