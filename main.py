dbd52dcc90d29161486d9c9496a8cf04c3cfb55b
Aminu Muhammad <105592867+Elvmeen@users.noreply.github.com>
2025-01-03 00:45:21 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-from func import post_generated_tweet  # Import the function that generates and posts the tweet
+from func import generate_prompt, post_tweet  # Import the necessary functions from func.py
 
 if __name__ == "__main__":
-    post_generated_tweet()  # Call the function to generate and post the tweet
+    # Generate a random tweet prompt
+    tweet = generate_prompt()
+
+    # Post the generated tweet using the Twitter API
+    post_tweet(tweet)  # Now it will work without passing any extra arguments
